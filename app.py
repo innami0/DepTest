7665bedd44ae78fd75f83470c9ebb9860f6be70d
innami0 <138112402+innami0@users.noreply.github.com>
2024-04-08 23:40:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask
-
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
-     return "<p>Hello, World!</p>"
+    return 'Hello World!'
 
 app.run(port=3000, debug=True)
